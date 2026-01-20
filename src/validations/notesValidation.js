daa2fc73c87ae59d35035eda711e134b250e8274
VitaKoval27 <talisman1980@gmail.com>
2026-01-20 11:09:27 +0000

    fixed tagValidation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -43,8 +43,6 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
     content: Joi.string().allow(''),
-    tag: Joi.string()
-      .valid(...TAGS)
-      .min(1),
+    tag: Joi.string().valid(...TAGS),
   }).min(1),
 };
